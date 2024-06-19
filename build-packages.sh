dd4ab8a44b6395388e8f4ddfc9d018b91a94e5d2
Ap <94882246+UnderGamer05@users.noreply.github.com>
2024-06-19 16:13:12 +0600

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -57,8 +57,9 @@ proot-distro login ubuntu <<-EOF
   unzip Godot_v4.2.2-stable_linux.arm32.zip
   chmod +x Godot_v4.2.2-stable_linux.arm32
   rm Godot_v4.2.2-stable_linux.arm32.zip
+  
+  ~/godot/Godot_v4.2.2-stable_linux.arm32 --export-release "Android" ./name.apk --headless
 
-  mkdir -p ~/.config/godot/
   mv ~/build-game/editor_settings-4.tres ~/.config/godot/
   exit
 EOF
